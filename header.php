0bc11e91522e7bff48b4b642d7e05aaec1e8535e
Ignacio Reyna <ignacioreyna56@gmail.com>
2019-03-15 00:28:10 -0600

    Framework

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<!-- Facebook tagging -->
+		<meta property="fb:app_id" content=""/>
+		<meta property="og:type"   content="website" />
+		<meta property="og:url"    content="<?php bloginfo('url')?>" />
+		<meta property="og:title"  content="<?php bloginfo('name')?>"/>
+		<meta property="og:image"  content="" />
+		<meta property="og:description"  content="<?php bloginfo('description')?>"/>
+		<!-- End of facebook tagging -->
+		<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1, user-scalable=no">
+		<meta name="description" content="<?php bloginfo('description')?>">
+		<meta name="keywords" content="">
+		<!--meta name="robots" content="INDEX,FOLLOW" NO SEO/-->
+		<link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url');?>/css/bootstrap.min.css">
+		<link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url');?>/css/generals.css">
+		<title><?php bloginfo('name')?></title>
+		<?php wp_head(); ?>
+	</head>
+	<body id="JaponSite">
